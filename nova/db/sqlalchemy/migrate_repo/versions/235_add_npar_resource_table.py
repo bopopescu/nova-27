944ffa4954e03bec717f4e245f1ead3a1624a1e1
Liu, Ning <ning.liu8@hp.com>
2014-10-09 18:29:12 +0800

    Card Name: Collect nPar info
    Add new DB table of nPar resource info and the migration test
    
    Description: New DB table for collected nPar resource info (cpu,
    memory, disk) designed, with migration test added.
    
    Author: Li Lei, Liu Ning

---
+++ nova/db/sqlalchemy/migrate_repo/versions/235_add_npar_resource_table.py
@@ -0,0 +1,63 @@
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import Integer
+from sqlalchemy import MetaData
+from sqlalchemy import String
+from sqlalchemy import Table
+
+from nova.openstack.common.gettextutils import _
+from nova.openstack.common import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+def upgrade(migrate_engine):
+    meta = MetaData()
+    meta.bind = migrate_engine
+
+    npar_resource = Table('nPar_resource', meta,
+        Column('created_at', DateTime),
+        Column('updated_at', DateTime),
+        Column('deleted_at', DateTime),
+        Column('npar_id', Integer, primary_key=True, nullable=False),
+        Column('ip_add', String(length=255), nullable=False),
+        Column('vcpus', Integer, nullable=True),
+        Column('vcpus_used', Integer, nullable=True),
+        Column('memory', Integer, nullable=True),
+        Column('memory_used', Integer, nullable=True),
+        Column('disk', Integer, nullable=True),
+        Column('disk_used', Integer, nullable=True),
+
+        mysql_engine='InnoDB',
+        mysql_charset='utf8'
+    )
+
+    try:
+        npar_resource.create()
+    except Exception:
+        LOG.info(repr(npar_resource))
+        LOG.exception(_('Exception while creating table.'))
+        raise
+
+
+def downgrade(migrate_engine):
+    meta = MetaData()
+    meta.bind = migrate_engine
+
+    table_name = ('nPar_resource')
+    table = Table(table_name, meta, autoload=True)
+    table.drop()
