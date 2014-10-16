bc1432182511de374b34bf6388336e7e76f2141a
Lei Li <li.lei2@hp.com>
2014-10-16 16:00:14 +0800

    Card Name: Spawn (After register)
    Description: Implement method exec_efi_cmd to utils, which aims
    at interacting with vPar console to support EFI shell execution.
    Also add utils test to ExecRemoteCmd.

--- nova/virt/hpux/utils.py
+++ nova/virt/hpux/utils.py
@@ -43,3 +43,35 @@ class ExecRemoteCmd(object):
             ssh.close()
 
         return execute_result
+
+    def exec_efi_cmd(self, **efi_cmd_info):
+        execute_result = None
+        # It needs two interaction to execute efi shell command for,
+        #   - 1) ssh to nPar to execute remote cmd 'vparconsole -p vpar_name'
+        #   - 2) type 'CO' to enter efi console and execute efi shell command
+        #        there
+        # Have a try to have no password ssh connection and replace password
+        # session with symbol 'CO', need real debug though!
+        try:
+            ssh = pexpect.spawn('ssh %s@%s "%s"' %
+                                (efi_cmd_info["username"],
+                                 efi_cmd_info["ip_address"],
+                                 efi_cmd_info["remote_command"]))
+            except_ret = ssh.expect([efi_cmd_info["vpar_name"]] + ' vMP>',
+                                    timeout=CONF.hpux.ssh_timeout_seconds)
+            if except_ret == 1:
+                ssh.sendline('CO')
+                ssh.expect("Shell>")
+                ssh.sendline(efi_cmd_info["efi_command"])
+            else:
+                raise exception.Invalid(_("failed on efi shell connection"))
+            execute_result = ssh.read()
+        except pexpect.EOF:
+            raise exception.Invalid(_("pexpect error EOF"))
+        except pexpect.TIMEOUT:
+            raise exception.Invalid(_("pexpect error TIMEOUT"))
+        except pexpect.ExceptionPexpect as e:
+            raise exception.Invalid(_("pexpect error UNKNOWN"))
+        finally:
+            ssh.close()
+        return execute_result
